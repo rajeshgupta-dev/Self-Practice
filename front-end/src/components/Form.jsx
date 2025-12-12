52d630c11d9523bb55d489e0c7d53d5efbe31b12
rajeshgu <rajeshlkh2020@gmail.com>
2025-12-12 22:45:53 +0530

    Add form

--- front-end/src/components/Form.jsx
+++ front-end/src/components/Form.jsx
@@ -4,12 +4,22 @@ import { Button } from './ui/button'
 
 const Form = () => {
   return (
-    <div className='flex items-center'>
-      <div className='grid w-full max-w-sm items-center gap-3'>
-        <Input type="email" placeholder="Enter email" />
-        <Button variant="outline">Button</Button>
+    <div className='flex items-center justify-center bg-zinc-100 w-full h-screen'>
+      <div className='max-w-sm flex flex-col items-center gap-3 bg-zinc-200 p-4 rounded-2xl'>
+        <h1 className='text-center m-4 text-3xl'>Login</h1>
+        <div className='w-full flex flex-col gap-3'>
+          <Input
+            className="focus:ring-2 focus:ring-red-500"
+            placeholder="Email"
+          />
+
+          <Button className="w-full bg-zinc-300 mt-1" variant="outline">
+            Button
+          </Button>
+        </div>
       </div>
     </div>
   )
 }
+
 export default Form
